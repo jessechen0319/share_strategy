e350ba3c05e0963bcd0514a056773b85269379e9
Chen <ruixiang.chen@hpe.com>
2017-10-09 17:34:44 +0800

    index.ejs v1

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+const jsonfile = require('jsonfile');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '股票策略记录验证系统'});
+});
+
+router.get('/tree', function(req, res, next) {
+  let treeJson = jsonfile.readFileSync(__dirname+"/DB/tree.json");
+  res.json(treeJson);
 });
 
 module.exports = router;
